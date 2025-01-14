7d9a8399f67b9537dfb0dcd5df4785bc383e6ddd
richardxavierpious <richardpious369@gmail.com>
2025-01-14 11:47:13 +0530

    local file transcription done

--- assemblyai/transcriber.py
+++ assemblyai/transcriber.py
@@ -4,12 +4,22 @@ aai.settings.api_key = "5c27bcc19e0642a38ad9842281185dbe"
 
 transcriber = aai.Transcriber()
 
-audio_file = "./../sampledata/memories.mp3"
+# You can use a local filepath:
+# audio_file = "./example.mp3"
 
-transcript = transcriber.transcribe(audio_file)
+# Or use a publicly-accessible URL:
+audio_file = "C:\\Users\\richa\\OneDrive\\Desktop\\Repos\\EchoScribe\\assemblyai\\test1.mp3"
 
-if transcript.error:
-   print(transcript.error)
-   exit(1)
+
+config = aai.TranscriptionConfig(speaker_labels=True, speakers_expected=3)
+
+transcript = transcriber.transcribe(audio_file, config)
+
+if transcript.status == aai.TranscriptStatus.error:
+    print(f"Transcription failed: {transcript.error}")
+    exit(1)
 
 print(transcript.text)
+
+for utterance in transcript.utterances:
+    print(f"Speaker {utterance.speaker}: {utterance.text}")
